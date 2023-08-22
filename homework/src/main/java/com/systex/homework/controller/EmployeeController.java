513a28924346f91fdc5e526b28fe4e72d8f89d52
EnWeiChang1107 <134591611+EnWeiChang1107@users.noreply.github.com>
2023-08-22 17:45:26 +0800

    Delete homework directory

--- homework/src/main/java/com/systex/homework/controller/EmployeeController.java
+++
@@ -1,9 +0,0 @@
-package com.systex.homework.controller;
-
-import com.systex.homework.entity.Employee;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class EmployeeController {
-
-}
--- homework/src/main/java/com/systex/homework/dao/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.systex.homework.dao;
-
-import com.systex.homework.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
