2e2b8475107e01d9105aa7ec6a7c2ed086a6c7f8
enwei <enwei1107@gmail.com>
2023-08-23 15:34:28 +0800

    no message

--- homework/src/main/java/com/systex/homework/dao/CategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.systex.homework.dao;
-
-import com.systex.homework.entity.Categories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriesRepository extends JpaRepository<Categories,Integer> {
-}
--- homework/src/main/java/com/systex/homework/dao/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.systex.homework.dao;
-
-import com.systex.homework.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
