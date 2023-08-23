1398d8a4fe7c239562cefbc672088115486433b7
enwei <enwei1107@gmail.com>
2023-08-23 16:20:55 +0800

    no message

--- homework/src/main/java/com/systex/homework/controller/ProductController.java
+++ homework/src/main/java/com/systex/homework/controller/ProductController.java
@@ -1,5 +1,16 @@
 package com.systex.homework.controller;
 
-@Rest
+import com.systex.homework.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+    private ProductService productService;
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService=productService;
+    }
 }
